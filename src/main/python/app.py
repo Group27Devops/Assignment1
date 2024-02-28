9e9b9227cff95aee6cee086f2034cb93ec3ef122
Group27Devops <2023mt03130@wilp.bits-pilani.ac.in>
2024-02-28 10:49:57 +0530

    made changes for passing lint and build stages - Logesh

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,9 +1,10 @@
-"""Module defined for using Flask framework"""
-from flask import Flask, render_template, request
-"""Module imported for using http requests"""
+"""Modules defined for using requests lib and Flask framework"""
 import requests
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+BASE_URL = "https://www.themealdb.com/api/json/v1"
 
 @app.route('/')
 def index():
@@ -13,19 +14,18 @@ def index():
 @app.route('/dishes')
 def dishes():
     """Function defined for rendering dishes.html"""
-    response = requests.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    response = requests.get(f'{BASE_URL}/1/filter.php?c=Seafood', timeout=50)
     data = response.json()
     return render_template('dishes.html', dishes=data['meals'])
 
-@app.route('/recipe/<reciperecipeId>')
-def recipe(recipeId):
+@app.route('/recipe/<recipe_id>')
+def recipe_dish(recipe_id):
     """Function defined for rendering recipe.html"""
-    response = requests.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={recipeId}')
+    response = requests.get(f'{BASE_URL}/1/lookup.php?i={recipe_id}', timeout=50)
     data = response.json()
     recipe = data['meals'][0]
     ingredients = [recipe[f'strIngredient{i}'] for i in range(1, 21) if recipe[f'strIngredient{i}']]
     return render_template('recipe.html', recipe=recipe, ingredients=ingredients)
 
 if __name__ == '__main__':
-    """Here is the main app initialization"""
     app.run(debug=True)
